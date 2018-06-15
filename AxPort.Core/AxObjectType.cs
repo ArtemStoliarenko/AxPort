b2dca247b605efd6fc4439a288067b8ef8ee2a58
EUROPE\v-arstol <v-arstol@microsoft.com>
2018-06-15 14:05:57 +0300

    Initialized repository with solution

---
+++ AxPort.Core/AxObjectType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxPort.Core
+{
+    public enum AxObjectType
+    {
+    }
+}
